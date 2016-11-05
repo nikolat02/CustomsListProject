b873a374c0b542925679c83068d411b5d4ed668f
ntirnanich <ntirnanich@yahoo.com>
2016-11-05 14:06:42 -0500

    Add files via upload

---
+++ List.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomsListProject
+{
+    public class CustomList<T> : IEnumerable
+    {
+        private T[] array;
+        private int count;
+
+        public CustomList()
+        {
+            array = new T[10];
+            count = 0;
+        }
+
+        public void Add(T an)
+        {
+            array[count] = an;
+            count = count + 1;
+        }
+
+        public int GetLength()
+        {
+            return count;
+        }
+
+        public bool Remove(T removeobj)  //Comeback to it
+        {
+            int index = Find(removeobj);
+            if (index < 0)
+            {
+                return false;
+            }
+
+            for (int i = index + 1; i < count; i++)
+            {
+                array[i - 1] = array[i];
+            }
+            count--;
+            return true;
+
+        }
+
+        public T Get(int index)
+        {
+            if (index < count && index >= 0)
+            {
+                return array[index];
+            }
+            else { return default(T); }
+        }
+
+        private int Find(T findobj)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (findobj.Equals(array[i]))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int index = 0; index < count; index++)
+            {
+                yield return array[index];
+            }
+
+        }
+
+        public CustomList<string> Zip(CustomList <T> List1, CustomList <T> List2)
+        {
+            CustomList<T> shortlist;
+            CustomList<T> longlist;
+            CustomList<string> combinedlist = new CustomList<string>();
+
+            if (List1.GetLength() > List2.GetLength())
+            {
+                longlist = List1;
+                shortlist = List2;
+            }
+
+            else
+            {
+                longlist = List2;
+                shortlist = List1;
+            }
+
+            for (int i = 0; i < longlist.GetLength(); i++)
+            {
+                if (i >= shortlist.GetLength())
+                {
+                    combinedlist.Add((dynamic)longlist.Get(i).ToString());
+                }
+                else
+                {
+                    combinedlist.Add((dynamic)longlist.Get(i).ToString() + (dynamic)shortlist.Get(i).ToString());
+                }
+            }
+            return combinedlist;
+        }
+
+        public static CustomList <T>operator +(CustomList<T> c1, CustomList<T> c2)
+        {
+            CustomList<T> shortlist;
+            CustomList<T> longlist;
+            CustomList<T> combinedlist = new CustomList<T>();
+
+            if (c1.GetLength() > c2.GetLength())
+            {
+                longlist = c1;
+                shortlist = c2;
+            }
+
+            else
+            {
+                longlist = c2;
+                shortlist = c1;
+            }
+
+            for (int i = 0; i < longlist.GetLength(); i++)
+            {
+                if (i >= shortlist.GetLength())
+                {
+                    combinedlist.Add(longlist.Get(i));
+                }
+                else
+                {
+                    combinedlist.Add((dynamic)longlist.Get(i) + (dynamic)shortlist.Get(i));
+                }
+            }
+            return combinedlist;
+
+        }
+
+        public static CustomList<T> operator -(CustomList<T> c1, CustomList<T> c2)
+        {
+            CustomList<T> shortlist;
+            CustomList<T> longlist;
+            CustomList<T> combinedlist = new CustomList<T>();
+
+            if (c1.GetLength() > c2.GetLength())
+            {
+                longlist = c1;
+                shortlist = c2;
+            }
+
+            else
+            {
+                longlist = c2;
+                shortlist = c1;
+            }
+
+            for (int i = 0; i < longlist.GetLength(); i++)
+            {
+                if (i >= shortlist.GetLength())
+                {
+                    combinedlist.Add(longlist.Get(i));
+                }
+                else
+                {
+                    combinedlist.Add((dynamic)longlist.Get(i) - (dynamic)shortlist.Get(i));
+                }
+            }
+            return combinedlist;
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomsListProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomList<int> myIntList = new CustomList<int>();
+            CustomList<int> myIntList2 = new CustomList<int>();
+            myIntList.Add(1);
+            myIntList.Add(2);
+            myIntList.Add(3);
+            myIntList2.Add(1);
+            myIntList2.Add(2);
+            myIntList2.Add(3);
+            CustomList<int> myTempIntList = myIntList + myIntList2;
+            myTempIntList = myIntList - myIntList2;
+            int j = myIntList.Get(0);
+            foreach (int temp in myIntList) { Console.WriteLine(temp); }
+            CustomList<string> tempList = new CustomList<string>();
+            Console.ReadKey();
+        }
+
+    }
+}
